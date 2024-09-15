7a8350a6175e7b52543cd508a5be476f1278af19
Msiza-VL <lindokehlemsiza.00@gmail.com>
2024-09-15 19:06:20 +0200

    Operating arithmetic operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,8 @@
-
+number1 = 10
+number2 = 5
+Addition = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1 * number2
+print("Addition of ", number1, "and", number2, Addition)
+print("Subtraction of ", number1, "and" , number2, Subtraction)
+print("Multiplication of ", number1, "and" , number2, Multiplication)
