e38a2ba204d48c53afa1cb1b7d23f3a73a791ff8
Msiza-VL <lindokehlemsiza.00@gmail.com>
2024-09-20 18:33:34 +0200

    Add different operators

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,1 +1,21 @@
-
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+match operation:
+   case '+':
+result = num1 + num2
+   case '-':
+result = num1 - num2
+   case '*':
+result = num1 * num2
+   case '/':
+if num2 == 0:
+    result = "Cannot divide by zero."
+else:
+    result = num1 / num2
+   case_:
+   result = "Invalid operation."
+if required(result, str):
+    print(result)
+else:
+    print("The result is" , result ".")
