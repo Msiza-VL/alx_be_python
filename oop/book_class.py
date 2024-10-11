e3f1ca714a847f80f7a59f57b54e04f6458bc384
Msiza-VL <lindokehlemsiza.00@gmail.com>
2024-10-11 18:59:44 +0200

    creating a Book instance and utilizing the implemented magic methods

---
+++ oop/book_class.py
@@ -0,0 +1,27 @@
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """Constructor to initialize the Book instance."""
+        self.title = title
+        self.author = author
+        self.year = year
+    def __del__(self):
+        """Destructor that prints a message when the object is deleted."""
+        print(f"Deleting {self.title}")
+    def __str__(self) -> str:
+        """String representation of the Book instance."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+    def __repr__(self) -> str:
+        """Official representation of the Book instance."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+from book_class import Book
+def main():
+    my_book = Book("1984", "George Orwell", 1949)
+
+    print(my_book)  
+
+    print(repr(my_book))  
+
+    del my_book
+
+if __name__ == "__main__":
+    main()
